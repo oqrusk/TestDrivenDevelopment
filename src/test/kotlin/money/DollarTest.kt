c9a68567fe8ac260278f3fd9accd178cd4e02d8f
oqrusk <y.okura@timers-inc.com>
2017-11-14 19:19:30 +0900

    complete chapter 2

--- src/test/kotlin/money/DollarTest.kt
+++ src/test/kotlin/money/DollarTest.kt
@@ -8,7 +8,10 @@ class DollarTest {
     @Test
     fun testMultiplication() {
         val five = Dollar(5)
-        five.times(2)
-        assertEquals(10, five.amount); }
+        var product = five.times(2)
+        assertEquals(10, product?.amount)
+        product = five.times(3)
+        assertEquals(15, product?.amount)
+    }
 }
 
