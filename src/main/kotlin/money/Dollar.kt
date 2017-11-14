1250bf1e303ca5baf56e839858e0833ba5d12c14
oqrusk <y.okura@timers-inc.com>
2017-11-14 19:45:46 +0900

    implement in java way

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -4,8 +4,16 @@ class Dollar constructor(amount: Int) {
 
     var amount:Int = amount
 
-    fun times(multiplier: Int) {
-        this.amount *= multiplier
-    }
+    fun times(multiplier: Int) : Dollar = Dollar(amount * multiplier)
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Dollar
+
+        if (amount != other.amount) return false
+
+        return true
+    }
 }
--- src/test/kotlin/money/DollarTest.kt
+++ src/test/kotlin/money/DollarTest.kt
@@ -8,7 +8,16 @@ class DollarTest {
     @Test
     fun testMultiplication() {
         val five = Dollar(5)
-        five.times(2)
-        assertEquals(10, five.amount); }
+        var product = five.times(2)
+        assertEquals(10, product?.amount)
+        product = five.times(3)
+        assertEquals(15, product?.amount)
+    }
+
+    @Test
+    fun testEquality(){
+        assertTrue(Dollar(5) == Dollar(5))
+        assertFalse(Dollar(5) == Dollar(6))
+    }
 }
 
