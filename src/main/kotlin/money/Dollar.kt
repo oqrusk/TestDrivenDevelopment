fe1504522d765327fdf1e8570524c887314f8beb
oqrusk <y.okura@timers-inc.com>
2017-11-14 19:46:32 +0900

    implement in kotlin way

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -1,19 +1,5 @@
 package money
 
-class Dollar constructor(amount: Int) {
-
-    var amount:Int = amount
-
+data class Dollar constructor(val amount: Int) {
     fun times(multiplier: Int) : Dollar = Dollar(amount * multiplier)
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Dollar
-
-        if (amount != other.amount) return false
-
-        return true
-    }
 }
