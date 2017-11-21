0532a488c9e502632c8115ff8168dc6eca664a05
oqrusk <y.okura@timers-inc.com>
2017-11-21 10:49:52 +0900

    complete chapter 8

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -2,6 +2,6 @@ package money
 
 class Dollar constructor(amount: Int) : Money(amount) {
 
-    fun times(multiplier: Int): Dollar = Dollar(amount * multiplier)
+    fun times(multiplier: Int): Money = Dollar(amount * multiplier)
 
 }
--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -3,6 +3,11 @@ package money
 open class Money(amount: Int) {
     internal var amount: Int = amount
 
+    companion object {
+        fun dollar(amount: Int): Dollar = Dollar(amount)
+        fun franc(amount: Int): Franc = Franc(amount)
+    }
+
     override fun equals(other: Any?): Boolean {
         val money = other as? Money ?: return false
 
